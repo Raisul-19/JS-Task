9d076f65d43db86b03860250051bac9de01ee553
Raisul-19 <raisul15-14119@diu.edu.bd>
2022-03-18 01:53:28 +0600

    js task on chepter-2 . (30:38)min

--- chapter-2/sandbox.js
+++ chapter-2/sandbox.js
@@ -1,3 +1,71 @@
-alert('hello, world!');
+
+
 console.log(1);
 console.log(2);
+
+
+let age = 25;
+let year = 2022;
+
+console.log(age, year);
+
+age = 30; //over write a variable.
+console.log(age);
+
+const points = 100; //constant can not be changed
+
+console.log(points);
+
+var score = 75;
+
+console.log(score);
+
+/*  data type of js
+number : 1, 46, 2.21 ,
+string : 'hello' , "hridoy" , "email@gamil.com" "124" ,
+boolean : true/false.
+null : explicitly set with no value.
+undefine : for variables that have not yet define.
+object :  complex data structures - array , dates, literals etc.
+symbol : used with object.
+*/
+
+// string  
+
+console.log("my name is Md Raisul Islam");
+
+let email = 'hridoy@gmail.com';
+console.log(email);
+
+// string concatenation or adding two string
+
+let firstName = "Raisul";
+let lastName = "Islam";
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+// getting charecter .
+
+console.log(fullName[0]);
+
+// string length
+
+console.log(fullName.length);
+
+//  string methods
+
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+
+
+let index = email.indexOf('@'); // finding index number
+console.log(index);
+
+
+
+
+
+
+
